f68103ac8b3923c74efeab8f08585e42f933ca82
Abheek Dhawan <abheekd@protonmail.com>
2023-01-02 18:16:40 -0600

    feat: correct cooldown message once complete

--- src/main/java/com/abheekd/apcsbot/listeners/commands/GetHelpCommand.java
+++ src/main/java/com/abheekd/apcsbot/listeners/commands/GetHelpCommand.java
@@ -5,10 +5,12 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageEditBuilder;
 
 import java.awt.*;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class GetHelpCommand extends ListenerAdapter {
     static HashMap<String, Date> cooldown = new HashMap<>();
@@ -22,6 +24,8 @@ public class GetHelpCommand extends ListenerAdapter {
                 if (complete.after(now)) {
                     MessageEmbed embed = new EmbedBuilder().setColor(new Color(255, 50, 50)).setDescription(String.format("⌛ You can use this <t:%d:R> dumbass spammer", complete.getTime() / 1000)).build();
                     event.replyEmbeds(embed).queue();
+                    embed = new EmbedBuilder().setColor(new Color(25, 255, 100)).setDescription("✅ You can finally use this command again").build();
+                    event.getHook().editOriginal(new MessageEditBuilder().setEmbeds(embed).build()).queueAfter(complete.getTime() - now.getTime(), TimeUnit.MILLISECONDS);
                     return;
                 } else {
                     cooldown.remove(event.getUser().getId());
